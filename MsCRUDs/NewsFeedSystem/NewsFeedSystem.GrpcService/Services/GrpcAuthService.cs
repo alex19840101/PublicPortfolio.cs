41001257a98450d31a6457de5383db13f171d787
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-04-05 11:15:10 +0300

    gRPC services fundamentals, Program.cs development

--- MsCRUDs/NewsFeedSystem/NewsFeedSystem.GrpcService/Services/GrpcAuthService.cs
+++ MsCRUDs/NewsFeedSystem/NewsFeedSystem.GrpcService/Services/GrpcAuthService.cs
@@ -1,6 +1,8 @@
 ﻿using Grpc.Core;
+using NewsFeedSystem.Core.Auth;
 using NewsFeedSystem.Core.Services;
 using NewsFeedSystem.GrpcService;
+using NewsFeedSystem.GrpcService.Auth;
 
 namespace NewsFeedSystem.GrpcService.Services
 {
@@ -13,5 +15,34 @@ namespace NewsFeedSystem.GrpcService.Services
             _authService = authService;
             _logger = logger;
         }
+
+        public async Task<AuthReply> RegisterUser(RegisterUserRequest registerUserRequest)
+        {
+            
+            var authUserToRegister = new AuthUser(
+                id: 0,
+                login: registerUserRequest.Login,
+                userName: registerUserRequest.UserName,
+                email: registerUserRequest.Email,
+                passwordHash: SHA256Hasher.GeneratePasswordHash(registerUserRequest.Password, registerUserRequest.RepeatPassword),
+                nick: registerUserRequest.Nick,
+                phone: registerUserRequest.Phone,
+                role: registerUserRequest.RequestedRole,
+                granterId: null,
+                createdDt: DateTime.Now,
+                lastUpdateDt: null);
+
+            var registerResult = await _authService.Register(authUserToRegister);
+
+            return new AuthReply
+            {
+                Id = registerResult.Id,
+                Message = registerResult.Message,
+                StatusCode = (int)registerResult.StatusCode
+            };
+        }
+
+
+
     }
 }
