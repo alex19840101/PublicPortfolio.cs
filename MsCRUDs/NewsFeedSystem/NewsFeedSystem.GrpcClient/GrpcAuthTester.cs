db5578d886318cf9e9b7236f01eb085e2e3015a6
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-04-06 12:01:29 +0300

    MsCRUDs\NewsFeedSystem\NewsFeedSystem.GrpcClient development

--- MsCRUDs/NewsFeedSystem/NewsFeedSystem.GrpcClient/GrpcAuthTester.cs
+++ MsCRUDs/NewsFeedSystem/NewsFeedSystem.GrpcClient/GrpcAuthTester.cs
@@ -11,6 +11,7 @@ namespace NewsFeedSystem.GrpcClient
     internal class GrpcAuthTester
     {
         private readonly GrpcAuth.GrpcAuthClient _authClient;
+        private const byte DEADLINE_SECONDS = 3;
 
         internal GrpcAuthTester(GrpcAuth.GrpcAuthClient authClient)
         {
@@ -19,18 +20,58 @@ namespace NewsFeedSystem.GrpcClient
 
         internal async Task MakeTests()
         {
+            
+            Console.WriteLine("Enter GranterPassword:");
+            Console.ForegroundColor = ConsoleColor.Black;
+            var granterPassword = Console.ReadLine();
+
             var getUserInfoByIdRequest = new GetUserInfoByIdRequest { Id = 1 };
             await TestGetUserInfoByIdAsync(getUserInfoByIdRequest);
-
+            var granterLogin = "login";
             var getUserInfoByLoginRequest = new GetUserInfoByLoginRequest { Login = "login" };
             await TestGetUserInfoByLoginAsync(getUserInfoByLoginRequest);
+            
+            //Register
+            var testLoginName = $"User{DateTime.Now.Date}";
+            var pass = Guid.NewGuid().ToString();
+            var registerUserRequest = new RegisterUserRequest
+            {
+                Login = testLoginName,
+                UserName = testLoginName,
+                Email = "{testLoginName}@404.fake".Replace(" ", ""),
+                Password = pass,
+                RepeatPassword = pass,
+                Nick = null,
+                Phone = null,
+                RequestedRole = null
+            };
+            var userId = await TestRegisterUserAsync(registerUserRequest);
+
+            //GrantRoleToUser
+            var grantRoleRequest = new GrantRoleRequest
+            {
+                Id = (uint)userId,
+                Login = registerUserRequest.Login,
+                NewRole = "fakeRole",
+                GranterId = 1,
+                GranterLogin = granterLogin,
+                GranterPassword = granterPassword
+            };
+            await TestGrantRoleToUserAsync(grantRoleRequest);
+
         }
 
-        private async Task TestGetUserInfoByIdAsync(GetUserInfoByIdRequest getUserInfoByIdRequest)
+        private async Task TestGetUserInfoByIdAsync(GetUserInfoByIdRequest getUserInfoByIdRequest, string? jwt = null)
         {
             try
             {
-                var reply = await _authClient.GetUserInfoByIdAsync(getUserInfoByIdRequest);
+                var headers = RequestHeadersPreparator.GetMetadataWithAuthorizationHeader(jwt);
+                
+                var reply = await _authClient.GetUserInfoByIdAsync(
+                    getUserInfoByIdRequest,
+                    headers,
+                    deadline: DateTime.UtcNow.AddSeconds(DEADLINE_SECONDS));
+
                 ConsoleLogger.InfoOkMessage($"{reply.Id} | {reply.UserName} | {reply.Login}");
             }
             catch (RpcException rpcException)
@@ -43,11 +84,17 @@ namespace NewsFeedSystem.GrpcClient
             }
         }
 
-        private async Task TestGetUserInfoByLoginAsync(GetUserInfoByLoginRequest getUserInfoByLoginRequest)
+        private async Task TestGetUserInfoByLoginAsync(GetUserInfoByLoginRequest getUserInfoByLoginRequest, string? jwt = null)
         {
             try
             {
-                var reply = await _authClient.GetUserInfoByLoginAsync(getUserInfoByLoginRequest);
+                var headers = RequestHeadersPreparator.GetMetadataWithAuthorizationHeader(jwt);
+
+                var reply = await _authClient.GetUserInfoByLoginAsync(
+                    getUserInfoByLoginRequest,
+                    headers,
+                    deadline: DateTime.UtcNow.AddSeconds(DEADLINE_SECONDS));
+
                 ConsoleLogger.InfoOkMessage($"{reply.Id} | {reply.UserName} | {reply.Login}");
             }
             catch (RpcException rpcException)
@@ -57,6 +104,54 @@ namespace NewsFeedSystem.GrpcClient
             catch (Exception ex)
             {
                 ConsoleLogger.Error(ex);
+            }
+        }
+
+        private async Task TestGrantRoleToUserAsync(GrantRoleRequest grantRoleRequest, string? jwt = null)
+        {
+            try
+            {
+                var headers = RequestHeadersPreparator.GetMetadataWithAuthorizationHeader(jwt);
+
+                var reply = await _authClient.GrantRoleToUserAsync(
+                    grantRoleRequest,
+                    headers,
+                    deadline: DateTime.UtcNow.AddSeconds(DEADLINE_SECONDS));
+
+                ConsoleLogger.InfoOkMessage($"{reply.StatusCode} {reply.Message}");
+            }
+            catch (RpcException rpcException)
+            {
+                ConsoleLogger.Error(rpcException);
+            }
+            catch (Exception ex)
+            {
+                ConsoleLogger.Error(ex);
+            }
+        }
+
+
+        private async Task<uint?> TestRegisterUserAsync(RegisterUserRequest registerUserRequest)
+        {
+            try
+            {
+                var reply = await _authClient.RegisterUserAsync(
+                    registerUserRequest,
+                    deadline: DateTime.UtcNow.AddSeconds(DEADLINE_SECONDS));
+
+                ConsoleLogger.InfoOkMessage($"{reply.Id} {reply.StatusCode} {reply.Message} Token: {reply.Token}");
+                
+                return reply.Id;
+            }
+            catch (RpcException rpcException)
+            {
+                ConsoleLogger.Error(rpcException);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                ConsoleLogger.Error(ex);
+                return null;
             }
         }
     }
---
+++ MsCRUDs/NewsFeedSystem/NewsFeedSystem.GrpcClient/RequestHeadersPreparator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Core;
+
+namespace NewsFeedSystem.GrpcClient
+{
+    internal static class RequestHeadersPreparator
+    {
+        internal static Metadata? GetMetadataWithAuthorizationHeader(string? jwt)
+        {
+            if (jwt == null)
+                return null;
+
+            var requestHeaders = new Metadata
+            {
+                { "Authorization", $"Bearer {jwt}" }
+            };
+            
+            return requestHeaders;
+        }
+    }
+}
