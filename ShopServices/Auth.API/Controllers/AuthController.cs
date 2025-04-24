230a5b37e81fda97089c82d48eae79e397e548d2
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-04-24 12:01:27 +0300

    add empty ShopServices\Auth.API

---
+++ ShopServices/Auth.API/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Auth.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
