b2a38fc9742044c1c5f9bf8ad9b3f8898235c516
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-04-25 11:06:44 +0300

    add ShopServices.DataAccess project. AuthUser

---
+++ ShopServices/ShopServices.DataAccess/ShopServicesDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopServices.DataAccess.Configurations;
+using ShopServices.DataAccess.Entities;
+
+namespace ShopServices.DataAccess
+{
+    public class ShopServicesDbContext : DbContext
+    {
+        public ShopServicesDbContext(DbContextOptions<ShopServicesDbContext> options) : base(options)
+        {
+        
+        }
+
+        public DbSet<AuthUser> AuthUsers { get; set; }
+        //public DbSet<_> _s { get; set; }
+        //public DbSet<_> _s { get; set; }
+        //public DbSet<_> _s { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AuthUserConfiguration());
+            //modelBuilder.ApplyConfiguration(new _sConfiguration());
+            //modelBuilder.ApplyConfiguration(new _sConfiguration());
+            //modelBuilder.ApplyConfiguration(new _sConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
