7d8c6a76f21abaa2576e538fbf8dd1c0f948fe39
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-05-25 11:30:40 +0300

    ShopServices\Auth.API\Program.cs - builder.Services.AddApiVersioning(

--- ShopServices/Auth.API/Program.cs
+++ ShopServices/Auth.API/Program.cs
@@ -1,6 +1,8 @@
+using Asp.Versioning;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddApiVersioning(
+                    options =>
+                    {
+                        // reporting api versions will return the headers
+                        // "api-supported-versions" and "api-deprecated-versions"
+                        options.ReportApiVersions = true;
+
+                        options.Policies.Sunset(0.9)
+                                        .Effective(DateTimeOffset.Now.AddDays(60))
+                                        .Link("policy.html")
+                                            .Title("Versioning Policy")
+                                            .Type("text/html");
+                    })
+                //.AddMvc()
+                .AddApiExplorer(
+                    options =>
+                    {
+                        // add the versioned api explorer, which also adds IApiVersionDescriptionProvider service
+                        // note: the specified format code will format the version as "'v'major[.minor][-status]"
+                        options.GroupNameFormat = "'v'VVV";
+
+                        // note: this option is only necessary when versioning by url segment. the SubstitutionFormat
+                        // can also be used to control the format of the API version in route templates
+                        options.SubstituteApiVersionInUrl = true;
+                    });
 
 var app = builder.Build();
 
