fbf51a74e10ab75cbe8a28841e9cef4e9e41cf3b
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-05-28 17:09:06 +0300

    Repositories\I...Repository

---
+++ ShopServices/ShopServices.Core/Repositories/IEmailNotificationsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopServices.Core.Repositories
+{
+    internal interface IEmailNotificationsRepository
+    {
+    }
+}
