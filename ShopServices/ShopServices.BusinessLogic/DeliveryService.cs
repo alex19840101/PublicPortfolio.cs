aea5acb5f86be1b55efc300eff4c9a2bc26e3e53
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-06-08 09:37:58 +0300

    ShopServices - Delivery.API boilerplate - csproj, Program.cs, BL ctor, no Redis, appsettings.json

--- ShopServices/ShopServices.BusinessLogic/DeliveryService.cs
+++ ShopServices/ShopServices.BusinessLogic/DeliveryService.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using ShopServices.Core.Repositories;
 using ShopServices.Core.Services;
 
 namespace ShopServices.BusinessLogic
 {
     public class DeliveryService : IDeliveryService
     {
+        private readonly IDeliveryRepository _deliveryRepository;
+
+        public DeliveryService(IDeliveryRepository deliveryRepository)
+        {
+            _deliveryRepository = deliveryRepository;
+        }
     }
 }
