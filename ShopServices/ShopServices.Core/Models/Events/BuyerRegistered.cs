b379f9077c61b4dcb13216c0abecb05c46a7d275
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-07-27 16:32:07 +0300

    Buyers - events, notifications

---
+++ ShopServices/ShopServices.Core/Models/Events/BuyerRegistered.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShopServices.Core.Models.Events
+{
+    /// <summary> Событие: покупатель зарегистрировался </summary>
+    public class BuyerRegistered : BuyerEventBase
+    {
+        /// <summary> Событие: покупатель зарегистрировался </summary>
+        public BuyerRegistered(Guid id, DateTime created, string message, string notification, uint buyerId) : base(id, created, message, notification, buyerId)
+        {
+        }
+    }
+}
