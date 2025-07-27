78b92bc79e79ead82d6de09b4eea77012316ec32
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-07-27 16:32:45 +0300

    Employees - events, notifications

---
+++ ShopServices/ShopServices.Core/Models/Events/EmployeeRegistered.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShopServices.Core.Models.Events
+{
+    /// <summary> Событие: работник зарегистрировался </summary>
+    public class EmployeeRegistered : EmployeeEventBase
+    {
+        /// <summary> Событие: работник зарегистрировался </summary>
+        public EmployeeRegistered(Guid id, DateTime created, string message, string notification, uint employeeId) : base(id, created, message, notification, employeeId)
+        {
+        }
+    }
+}
---
+++ ShopServices/ShopServices.Core/Models/Events/EmployeeUpdated.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ShopServices.Core.Models.Events
+{
+    /// <summary> Событие: работник изменил личные данные </summary>
+    public class EmployeeUpdated : EmployeeEventBase
+    {
+        /// <summary> Событие: работник изменил личные данные </summary>
+        public EmployeeUpdated(Guid id, DateTime created, string message, string notification, uint employeeId) : base(id, created, message, notification, employeeId)
+        {
+        }
+    }
+}
