c75f552195ecffd2c2a7f1f6ee75981c5f7845a5
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2025-12-17 11:16:32 +0300

    ShopServices\ShopServices.DataAccess\Migrations\20251217074429_Availability ...

--- ShopServices/ShopServices.DataAccess/Migrations/20251217074429_Availability.Designer.cs
+++ ShopServices/ShopServices.DataAccess/Migrations/20251217074429_Availability.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShopServices.DataAccess;
@@ -12,9 +13,11 @@ using ShopServices.DataAccess;
 namespace ShopServices.DataAccess.Migrations
 {
     [DbContext(typeof(ShopServicesDbContext))]
-    partial class ShopServicesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251217074429_Availability")]
+    partial class Availability
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ShopServices/ShopServices.DataAccess/Migrations/20251217074429_Availability.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopServices.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class Availability : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CityTownCode",
+                table: "Availabilities",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Availabilities",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastSupplyTime",
+                table: "Availabilities",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ManagerId",
+                table: "Availabilities",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NextSupplyTime",
+                table: "Availabilities",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PlaceName",
+                table: "Availabilities",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Updated",
+                table: "Availabilities",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Availabilities_ManagerId",
+                table: "Availabilities",
+                column: "ManagerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Availabilities_Managers_ManagerId",
+                table: "Availabilities",
+                column: "ManagerId",
+                principalTable: "Managers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Availabilities_Managers_ManagerId",
+                table: "Availabilities");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Availabilities_ManagerId",
+                table: "Availabilities");
+
+            migrationBuilder.DropColumn(
+                name: "CityTownCode",
+                table: "Availabilities");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Availabilities");
+
+            migrationBuilder.DropColumn(
+                name: "LastSupplyTime",
+                table: "Availabilities");
+
+            migrationBuilder.DropColumn(
+                name: "ManagerId",
+                table: "Availabilities");
+
+            migrationBuilder.DropColumn(
+                name: "NextSupplyTime",
+                table: "Availabilities");
+
+            migrationBuilder.DropColumn(
+                name: "PlaceName",
+                table: "Availabilities");
+
+            migrationBuilder.DropColumn(
+                name: "Updated",
+                table: "Availabilities");
+        }
+    }
+}
