6c926d230f943258748557e1e0a2188f3cef3096
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2026-01-29 16:29:06 +0300

    Add-Migration Trade

--- ShopServices/ShopServices.DataAccess/Migrations/20260129131600_Trade.Designer.cs
+++ ShopServices/ShopServices.DataAccess/Migrations/20260129131600_Trade.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using ShopServices.DataAccess;
@@ -12,9 +13,11 @@ using ShopServices.DataAccess;
 namespace ShopServices.DataAccess.Migrations
 {
     [DbContext(typeof(ShopServicesDbContext))]
-    partial class ShopServicesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260129131600_Trade")]
+    partial class Trade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
