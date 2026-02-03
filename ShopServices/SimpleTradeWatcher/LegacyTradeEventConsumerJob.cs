9ea1d9c4098096cc7b627b32762698b461960002
Shapovalov Alexey <shapovalov.alexey.32@yandex.ru>
2026-02-03 11:46:33 +0300

    SimpleTradeWatcher

---
+++ ShopServices/SimpleTradeWatcher/LegacyTradeEventConsumerJob.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ShopServices.Core.Models.Events;
+
+internal class LegacyTradeEventConsumerJob : BackgroundService
+{
+    private readonly ILogger<LegacyTradeEventConsumerJob> _logger;
+
+    public LegacyTradeEventConsumerJob(ILogger<LegacyTradeEventConsumerJob> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:29092",
+            GroupId = "test-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+        const int CONSUMER_TIMEOUT_SECONDS = 5;
+        using var consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+
+        consumer.Subscribe(EventsTopics.LEGACY_PAYMENT_RECIEVED);
+        consumer.Subscribe(EventsTopics.LEGACY_REFUND_RECIEVED);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            try
+            {
+                var consumeResult = consumer.Consume(TimeSpan.FromSeconds(CONSUMER_TIMEOUT_SECONDS));
+
+                if (consumeResult == null)
+                    continue;
+
+                _logger.LogInformation($"Offset:{consumeResult.Offset} MSG: {consumeResult.Message.Value}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
